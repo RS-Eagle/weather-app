08e8ce07ac1c7a0784c4cf8b080261e3f17ae644
Raman Kumar Charak <withrkgaming78@gmail.com>
2024-05-09 18:42:31 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cards from "./componenets/Cards";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useState, useRef } from "react";
 function App() {
+  const [numberr, setnumber] = useState([
+    "Kangra",
+    "Nurpur,himachal",
+    "Chamba,himachal",
+  ]);
+
+  const inputRef = useRef();
+  const changeHandler = (e) => {
+    const newValue = inputRef.current.value;
+    console.log(newValue);
+
+    setnumber([newValue]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="navbar">
+        <h2>Eagle-Weather</h2>
+        <div className="input-field">
+          <input type="text" ref={inputRef} placeholder="Enter Location" />
+          <FontAwesomeIcon
+            onClick={changeHandler}
+            className="icon"
+            icon={faSearch}
+          />
+        </div>
+
+        
+      </div>
+      <div className="main-data">
+        {numberr.map((i) => {
+          return <Cards name={i} key={i} />;
+        })}
+      </div>
+      ;
     </div>
   );
 }
---
+++ src/componenets/Cards.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./Cards.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTemperatureQuarter,
+  faWind,
+  faDroplet,
+} from "@fortawesome/free-solid-svg-icons";
+
+import Gif from "./../img/Plant.gif";
+
+const Cards = (props) => {
+  const [locationName, setlocationName] = useState("");
+  const [desc, setdesc] = useState("");
+  const [icon, seticon] = useState("");
+  const [temp, settemp] = useState("");
+  const [wind, setwind] = useState("");
+  const [error, seterror] = useState("");
+  const [hum, sethum] = useState("");
+  const [bol, setbol] = useState(false);
+  const [bol2, setbol2] = useState(true);
+
+  useEffect(() => {
+    setbol(false);
+    setbol2(true);
+    const params = {
+      key: "00f50a2641f04117bed75524232807",
+      q: props.name,
+    };
+
+    axios
+      .get("http://api.weatherapi.com/v1/current.json", { params })
+      .then((response) => {
+        setlocationName(response.data.location.name);
+        settemp(response.data.current.temp_c);
+        setdesc(response.data.current.condition.text);
+        seticon(response.data.current.condition.icon);
+        setwind(response.data.current.wind_mph);
+        sethum(response.data.current.humidity);
+        setbol(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        seterror(error.response.data.error.message);
+
+        setbol(true);
+        setbol2(false);
+      });
+  }, [props.name]);
+  return (
+    <>
+      {bol ? (
+        bol2 ? (
+          <div className="main-weather">
+            <div className="location">{locationName}</div>
+
+            <img src={icon} alt="" />
+            <div className="temprature">
+              <FontAwesomeIcon icon={faTemperatureQuarter} /> {"  "}
+              {temp}
+            </div>
+            <div className="des">{desc}</div>
+            <div className="moredetails">
+              <div className="wind">
+                <FontAwesomeIcon icon={faWind} />
+                {wind} Kmph
+              </div>
+              <div className="humidity">
+                <FontAwesomeIcon icon={faDroplet} />
+                {hum}
+              </div>
+            </div>
+          </div>
+        ) : (
+          <h1>{error}</h1>
+        )
+      ) : (
+        <div className="main-weather">
+          <img className="img1" src={Gif} alt="" />
+          <h2>Loading..</h2>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Cards;
